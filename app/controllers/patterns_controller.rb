ba3ec6ae882df6c24932c3a3314bc299ac315c34
Billiesnyc <billiesnyc@gmail.com>
2018-10-25 17:59:35 +0100

    add new pattern added

--- app/controllers/patterns_controller.rb
+++ app/controllers/patterns_controller.rb
@@ -4,13 +4,44 @@ class PatternsController < ApplicationController
         @patterns = Pattern.all
         render json: @patterns
       end
-    
-      def create
-        @pattern = Pattern.new(name: params[:name], image_url: params[:image_url], download_url: params[:download_url], difficulty: params[:difficulty], size: params[:size], user_id: params[:user_id])
-        if @pattern.save
+      
+      def show
+        @pattern = Pattern.find_by(id: params[:id])
+        if @pattern
           render json: @pattern
         else
-          render json: {error: 'Pattern failed to be created.'}, status: 400
+          render json: {error: 'Pattern not found.'}, status: 404
         end
+      end
+
+      def new
+        @pattern = Pattern.new
+      end
+
+      def create
+        @pattern = Pattern.create(pattern_params)
+        render json: @pattern
+        # if @pattern.save
+        #   render json: @pattern
+        # else
+        #   render json: {error: 'Pattern failed to be created.'}, status: 400
+        # end
+      end
+
+      def edit
+      end
+
+      def update
+      end
+
+      def destroy
+      end
+
+      private
+
+      def pattern_params
+
+        params.require(:pattern).permit(:name, :image_url, :download_url, :difficulty, :size, :likes, :user_id)
+      
       end
 end
